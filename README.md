a5a642ade64dc3f05bcd1d1dd6661a214d0ca554
Mikee <mikeelaurente9@gmail.com>
2025-11-03 00:31:28 +0800

    Update README and main.py: Add seed data instructions and clean up unused imports

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # CIT Capstone Repository
+
 A web-based capstone storage repository for CIT, written in Python with FastAPI<br>
 This also utilize smart searching with Sentence Transformer using model `all-MiniLM-L6-v2`
 
 ## How to use?
+
 1. Clone this repository
 2. Go to the working directory:
-	```
+   ```
    cd cit-capstone-repository
    ```
 3. Install required dependencies:
-	```
+   ```
    pip install -r requirements.txt
    ```
-4. Run the FastAPI Uvicorn server:
+4. Run seed data
+   ```
+   python seed.py
+   ```
+5. Run the FastAPI Uvicorn server:
    ```
    uvicorn main:app --reload
    ```
--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from sqlalchemy import String
-from sqlalchemy.orm import Session
-from helpers.embedding import encode_text, load_embedding
-from helpers.password import hash_password, verify_password
-from helpers.pdf import PdfHelper
-from helpers.session import get_current_user, require_role, require_role_frontend
-from models import Capstone, User
-from db import get_db
-from config import PathConfig, AuthConfig
+from config import PathConfig
 from modules.admin.capstones import configure_admin_capstone_module
 from modules.admin.users import configure_admin_users_module
 from modules.auth import configure_auth_module
 from modules.capstones import configure_capstone_module
 from modules.home import configure_home_module
-from repositories.user import UserRepository
 
 # ------------------------------
 # FastAPI app setup
