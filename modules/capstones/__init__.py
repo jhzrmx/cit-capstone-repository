f3e21d3331cbd95ea17d17a03ae08c3ab08d3c3f
Marlon S. Caisip <90949319+marlon-caisip@users.noreply.github.com>
2025-11-03 21:39:24 +0800

    Added api_get_summary route in capstones module initializer

--- modules/capstones/__init__.py
+++ modules/capstones/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from modules.capstones.api_search_capstones import register_api_search_capstones_route
+from modules.capstones.api_get_summary import register_api_get_summary_route
 from .get_capstone_overview import register_capstone_overview_route
 
 def configure_capstone_module(app: FastAPI):
     register_capstone_overview_route(app)
     register_api_search_capstones_route(app)
+    register_api_get_summary_route(app)
